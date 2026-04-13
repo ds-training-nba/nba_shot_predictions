ec654c6c8850ae6f0ccc37240da887eba6540db3
soeren <mail@floatingbitsmedia.de>
2026-04-13 16:44:54 +0200

    new comparison viz and shot accuracy feature

--- scripts/action_type_viz.py
+++ scripts/action_type_viz.py
@@ -10,14 +10,32 @@ from processing.preprocessing import shot_accuracy_by_fields, add_shot_main_acti
 # main
 df = main_dataframe()
 
-df = shot_accuracy_by_fields(df, ['Main Action Type', 'Player Name']).reset_index()
+df_accuracy = shot_accuracy_by_fields(df, ['Main Action Type', 'Player Name']).reset_index()
 
-g = sns.FacetGrid(df, col="Main Action Type", col_wrap=4, height=3)
+# g = sns.FacetGrid(df_accuracy, col="Main Action Type", col_wrap=4, height=3)
+#
+# g.map_dataframe(
+#     sns.barplot,
+#     x="Player Name",
+#     y=TARGET_VARIABLE
+# )
+
+# g = sns.FacetGrid(df_accuracy, col="Player Name", col_wrap=4, height=3)
+#
+# g.map_dataframe(
+#     sns.barplot,
+#     x="Main Action Type",
+#     y=TARGET_VARIABLE
+# )
+
+g = sns.FacetGrid(df, col="Player Name", col_wrap=4, height=3)
 
 g.map_dataframe(
-    sns.barplot,
-    x="Player Name",
-    y=TARGET_VARIABLE
+    sns.histplot,
+    x="Main Action Type",
+    stat="probability"
 )
+
+
 
 plt.show()
