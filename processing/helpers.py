f998ec34e25d22cb894bfdb6a8c1dd049dc7064f
soeren <mail@floatingbitsmedia.de>
2026-04-23 13:07:28 +0200

    refactoring shot accuracy helper function location

---
+++ processing/helpers.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+from app.config import TARGET_VARIABLE
+
+
+def shot_accuracy_by_fields(df: pd.DataFrame, fields):
+    grouped_df = df.groupby(fields).agg({TARGET_VARIABLE: "mean"})
+    return grouped_df
