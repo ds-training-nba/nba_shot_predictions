05a5977b12b96dd1eb380571bca307efccd611dc
soeren <mail@floatingbitsmedia.de>
2026-04-23 13:07:21 +0200

    refactoring shot accuracy helper function location

--- processing/preprocessing.py
+++ processing/preprocessing.py
@@ -1,11 +1,3 @@
-import pandas as pd
-from app.config import TARGET_VARIABLE
-
-def shot_accuracy_by_fields(df: pd.DataFrame, fields):
-    grouped_df = df.groupby(fields).agg({TARGET_VARIABLE: "mean"})
-    return grouped_df
-
-
 def preprocess_fields(df):
     
     # Remove rows with inconsistencies in the targets
--- scripts/stats_test.py
+++ scripts/stats_test.py
@@ -1,6 +1,6 @@
 from app.config import TARGET_VARIABLE
 from app.data_providers import get_shots_dataframe, main_dataframe, filtered_shots_dataframe
-from processing.preprocessing import shot_accuracy_by_fields
+from processing.helpers import shot_accuracy_by_fields
 from processing.compute_columns import add_shot_main_action_type_column
 
 # main
